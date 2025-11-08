7ec60f150d2b6dbe9fe62c905974314876e4d6a7
David Costa <deividesantoscosta@gmail.com>
2025-11-07 22:38:09 -0300

    a ia está confusa e sugeriu a mudança do trustproxie de novo

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -7,18 +7,7 @@ use Illuminate\Http\Request;
 
 class TrustProxies extends Middleware
 {
-    protected $proxies;
+    protected $proxies = '*';
+    protected $headers = Request::HEADER_X_FORWARDED_ALL;
 
-    protected $headers =
-        Request::HEADER_X_FORWARDED_FOR |
-        Request::HEADER_X_FORWARDED_HOST |
-        Request::HEADER_X_FORWARDED_PORT |
-        Request::HEADER_X_FORWARDED_PROTO |
-        Request::HEADER_X_FORWARDED_AWS_ELB;
-
-    public function __construct()
-    {
-        // Use '*' como padrão, mas deixa configurável via .env
-        $this->proxies = env('APP_TRUSTED_PROXIES', '*');
-    }
 }
